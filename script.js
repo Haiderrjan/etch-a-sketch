60fdb63fa70be63e9b685e1963c5fe9d4db9cc2c
Haider Jan <haider@imijs5t>
2025-10-07 16:43:06 +0100

    Added the button to reset to white. fixed the changning drop down list to other grid layout. added a button to have a differnt colour all the time if u want to use the same colour and not change to a different one.

--- script.js
+++ script.js
@@ -1,15 +1,64 @@
 const grid_Size = document.querySelector("#size");
+const btn = document.querySelector("#reset")
 
 
 
 
+const btn_2 = document.querySelector("#changecolour")
 
-const container = document.querySelector("#container")
+
+const column = document.createElement("div")
+
+
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container)
+
+
+
+for (let i = 0; i < 8; i++){
+    const row = document.createElement("div");
+    row.classList.add("row")
+    container.appendChild(row)
+    
+    for(let j = 0; j < 8; j++){
+        const column = document.createElement("div")
+        column.classList.add("column")
+        row.appendChild(column)
+
+
+        column.addEventListener("mouseenter", (event) => {
+
+column.style.backgroundColor = "red"
+})
+
+    }
+
+
+
+}
+
+
 
 
 grid_Size.addEventListener("change", function(){
 
+
 let size = parseInt(this.value)
+
+
+
+
+
+    const existingContainer = document.querySelector(".container")
+
+    if (existingContainer) {
+        existingContainer.remove()
+    }
+
+const container = document.createElement("div");
+container.classList.add("container");
+ document.body.appendChild(container)
 
 
 for (let i = 0; i < size; i++){
@@ -21,16 +70,69 @@ for (let i = 0; i < size; i++){
         const column = document.createElement("div")
         column.classList.add("column")
         row.appendChild(column)
+
+
+         column.addEventListener("mouseenter", (event) => {
+
+column.style.backgroundColor = "red"
+
+
+})
     }
 
 }
-
 
 })
 
 
 
 
+btn.addEventListener("click", function(){
 
-  
+cleaningGrid();
+})
 
+
+
+
+    btn_2.addEventListener("click", function(){
+        cleaningGrid();
+        
+        const newColour = randomColour();
+        console.log(newColour)
+
+  const columns = document.querySelectorAll(".column");
+        
+columns.forEach((column) => {
+    column.addEventListener("mouseenter", () => {
+      column.style.backgroundColor = newColour;
+    });
+  });
+});
+     
+
+function cleaningGrid(){
+
+     let columns = document.querySelectorAll(".column");
+
+columns.forEach((column) => {
+    column.style.backgroundColor = "white"
+
+})
+
+}
+
+
+
+
+const randomColour = ()  => {
+const R = Math.floor(Math.random() * 255) + 1;
+const G = Math.floor(Math.random() * 255) + 1;
+const B = Math.floor(Math.random() * 255) + 1;
+
+return `rgb(${R}, ${G}, ${B})`;
+}
+
+
+
+    
